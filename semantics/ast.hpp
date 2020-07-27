4cb8cef1f945a646e95eb39a5a5614e0d242a12e
SergeantChris <christina.sartzetaki@gmail.com>
2020-07-27 21:18:23 +0300

    Implementation up to Def class and subclasses

--- semantics/ast.hpp
+++ semantics/ast.hpp
@@ -2,24 +2,30 @@
 #define __AST_HPP__
 
 #include <iostream>
-#include <list>
+#include <vector>
+#include <string>
+#include "lexer.hpp"
 
 using namespace std;
-
-//yyerror?
 
 enum PrimitiveType { TYPE_int, TYPE_bool, TYPE_char };
 
 class CompositeType { //abstract class
-
+public:
+	virtual ~CompositeType() {}
+protected:
+	string id;
+	Type type;
 };
 
 class Array: public CompositeType {
-
+public:
+	Array(Type t): id("array"), type(t) {}
 };
 
 class List: public CompositeType {
-
+public:
+	List(Type t): id("list"), type(t) {}
 };
 
 union Type {
@@ -34,8 +40,8 @@ inline ostream& operator<<(ostream &out, Type t) {
 		case TYPE_bool: out << "bool"; break;
 		case TYPE_char: out << "char"; break;
 		switch((t.c)->id) {
-			case array: out << "array of" << ((t.c)->type); break;
-			case list: out << "list of" << ((t.c)->type); break;
+			case "array": out << "array of" << ((t.c)->type); break;
+			case "list": out << "list of" << ((t.c)->type); break;
 		}
 	}
 	return out;
@@ -45,6 +51,7 @@ class ASTnode { //abstract class
 public:
 	virtual ~ASTnode() {}
 	virtual void printNode(ostream &out) const = 0;
+	virtual void sem() {}
 };
 
 inline ostream& operator<<(ostream &out, const ASTnode &n) {
@@ -54,21 +61,62 @@ inline ostream& operator<<(ostream &out, const ASTnode &n) {
 
 class Def: public ASTnode { //abstract class
 public:
-
+	virtual ~Def() {}
 };
 
 class FuncDef: public Def {
 public:
-	
-
+	FuncDef(Header* h, vector<shared_ptr<Def>>* d, vector<shared_ptr<Stmt>>* s):
+		hd(h), defl(d), stmtl(s) {}
+	~FuncDef() { delete hd; delete defl; delete stmtl; }
+	virtual void printNode(ostream &out) const override {
+		out << "FuncDef(" << *hd << ", ";
+		bool first = true;
+		for(shared_ptr<Def> d: *defl) {
+			if(!first) out << ", ";
+			first = false;
+			out << *d;
+		}
+		for(shared_ptr<Stmt> s: *stmtl) {
+			if(!first) out << ", ";
+			first = false;
+			out << *s;
+		}
+		out << ")";
+	}
+private:
+	Header* hd;
+	vector<shared_ptr<Def>>* defl;
+	vector<shared_ptr<Stmt>>* stmtl;
 };
 
 class FuncDecl: public Def {
-
+public:
+	FuncDecl(Header* h): hd(h) {}
+	~FuncDecl() { delete hd; }
+	virtual void printNode(ostream &out) const override {
+		out << "FuncDecl(" << *hd << ")";
+	}
+private:
+	Header* hd;
 };
 
 class VarDef: public Def {
-
+public:
+	VarDef(Type t, vector<string>* i): type(t), idl(i) {}
+	~VarDef() { delete idl; }
+	virtual void printNode(ostream &out) const override {
+		out << "VarDef(" << type << ", ";
+		bool first = true;
+		for(string i: *idl) {
+			if(!first) out << ", ";
+			first = false;
+			out << i;
+		}
+	}
+private:
+	Type type;
+	vector<string>* idl;
 };
 
 class Header: public ASTnode {
@@ -108,7 +156,13 @@ class Loop: public Stmt {
 };
 
 class Expr: public ASTnode { //abstract class
-
+public:
+  void type_check(Type t) {
+    sem();
+    if (type != t) yyerror("Type mismatch");
+  }
+protected:
+  Type type;
 };
 
 class Atom: public Expr { //abstract class
